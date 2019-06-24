bb19b74c911b063c2c09c4e259ad6dc96659c1db
sheailun <sheailun@kingsoft.com>
2019-06-24 14:16:54 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       port: "22",
       ref: 'origin/master',
       repo: 'git@github.com:jf3096/pm2-remote-deploy-test.git',
+      'pre-deploy': 'git reset --hard',
       path: '/www'
     }
   }

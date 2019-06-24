58c2bfe649112ae7bfdcf068d11eb008b03b2ad7
sheailun <sheailun@kingsoft.com>
2019-06-24 14:06:21 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,20 @@
 module.exports = {
-    apps: [
-        {
-            name: 'API',
-            script: 'index.js',
-            args: 'one two',
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G',
-            env: {
-                NODE_ENV: 'development'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        }
-    ],
-
-    deploy: {
-        production: {
-            user: 'root',
-            host: '120.92.102.112',
-            port: "22",
-            ref: 'origin/master',
-            repo: 'git@github.com:jf3096/pm2-remote-deploy-test.git',
-            path: '/www',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-        }
+  apps: [
+    {
+      name: 'API',
+      script: 'index.js',
     }
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '120.92.102.112',
+      port: "22",
+      ref: 'origin/master',
+      repo: 'git@github.com:jf3096/pm2-remote-deploy-test.git',
+      path: '/www',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 };
